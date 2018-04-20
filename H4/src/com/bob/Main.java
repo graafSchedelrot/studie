46c73bd7e2fbf4d50b711f0406a7e51cc26e6c16
Bob Wijmenga <bob.wijmenga@rhea.infosupport.net>
2018-04-20 16:33:13 +0200

    Add H4

---
+++ H4/src/com/bob/Main.java
@@ -0,0 +1,11 @@
+package com.bob;
+
+import com.bob.frames.FrameMain;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FrameMain frame = new FrameMain();
+        frame.newFrame();
+    }
+}
---
+++ H4/src/com/bob/frames/FrameOefening.java
@@ -0,0 +1,16 @@
+package com.bob.frames;
+
+import javax.swing.*;
+
+public class FrameOefening extends JFrame {
+
+    public void newFrame(JPanel panel) {
+        JFrame frame = new FrameOefening();
+        frame.setLocation(500, 400);
+        frame.setSize(300, 400);
+        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        frame.setTitle("Oefening");
+        frame.setContentPane(panel);
+        frame.setVisible(true);
+    }
+}
